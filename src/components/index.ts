72097427246ef3163ed52777cf9200bd11f87e7b
Diecodev <diegoddiaz05@gmail.com>
2024-01-13 15:36:25 -0500

    refactor: 🏷️ export Toaster component props interface

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,7 @@
 export { Toaster } from "./toaster";
 export { toast } from "../utils/state";
-export type { ToastT, ExternalToast } from "../utils/types";
+export type {
+  ToastT as Toast,
+  ExternalToast,
+  ToasterProps,
+} from "../utils/types";

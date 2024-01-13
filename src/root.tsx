b6037a49996f6b41c2879847217b68432722e471
Diecodev <diegoddiaz05@gmail.com>
2024-01-13 08:08:42 -0500

    docs: 📝 change svelte-sonner to qwik-sonner

--- src/root.tsx
+++ src/root.tsx
@@ -1,3 +1,4 @@
+import { $ } from "@builder.io/qwik";
 import { Toaster, toast } from "./components";
 
 export default () => {
@@ -10,6 +11,25 @@ export default () => {
       <body>
         <button onClick$={() => toast.info("I'm a toast. Look at me")}>
           open toaster
+        </button>
+        <button onClick$={() => toast(<div>My custom toast</div>)}>
+          open toaster 2
+        </button>
+        <button
+          onClick$={() =>
+            toast("Event has been created", {
+              onDismiss: $((t) =>
+                console.log(`Toast with id ${t.id} has been dismissed`)
+              ),
+              onAutoClose: $((t) =>
+                console.log(
+                  `Toast with id ${t.id} has been closed automatically`
+                )
+              ),
+            })
+          }
+        >
+          open toaster 3
         </button>
         <Toaster />
       </body>

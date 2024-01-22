7cb381a7705c79501781146d326efd7d53f02a49
Diecodev <diegoddiaz05@gmail.com>
2024-01-22 05:09:07 -0500

    Revert "refactor: 🏷️ remove Node.Timeout type"
    This reverts commit c032595580f33e316fa18c46f06e53137cbb7314.

--- src/components/toast-card.tsx
+++ src/components/toast-card.tsx
@@ -148,7 +148,7 @@ export const Toast = component$((props: ToastProps) => {
       return;
     }
 
-    let timeoutId: number;
+    let timeoutId: number | NodeJS.Timeout;
     // Pause the timer on each hover
     const pauseTimer = () => {
       if (lastCloseTimerStartTimeRef.value < closeTimerStartTimeRef.value) {

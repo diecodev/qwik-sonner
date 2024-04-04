2e20aab6483f8c405e4a5d9c85655e385ca9753d
diecodev <diegoddiaz05@gmail.com>
2024-04-04 02:31:03 -0500

    build: 👷 update pkg.json scripts to handle new publish method

--- vite.config.ts
+++ vite.config.ts
@@ -2,20 +2,40 @@ import { defineConfig } from "vite";
 import pkg from "./package.json";
 import { qwikVite } from "@builder.io/qwik/optimizer";
 import tsconfigPaths from "vite-tsconfig-paths";
+import dts from "vite-plugin-dts";
 
 const { dependencies = {}, peerDependencies = {} } = pkg as any;
 const makeRegex = (dep) => new RegExp(`^${dep}(/.*)?$`);
 const excludeAll = (obj) => Object.keys(obj).map(makeRegex);
 
+const lib = {
+  raw: {
+    entry: "./src/index.ts",
+  },
+  headless: {
+    entry: "./src/headless/index.ts",
+  },
+} as const;
+
 export default defineConfig(() => {
+  const key = process.env.PKG ?? "raw";
+  if (!(key in lib)) throw new Error(`Invalid package: ${key}`);
+
+  const { entry } = lib[key as keyof typeof lib];
   return {
     build: {
       target: "es2020",
+      outDir: ".",
       lib: {
-        entry: "./src/index.ts",
+        entry,
         formats: ["es", "cjs"],
-        fileName: (format) => `index.qwik.${format === "es" ? "mjs" : "cjs"}`,
+        fileName: (format) => {
+          const name = key === "raw" ? "index" : `${key}/index`;
+          const ext = format === "es" ? "mjs" : "cjs";
+          return `${name}.qwik.${ext}`;
+        },
       },
+      emptyOutDir: false,
       rollupOptions: {
         // externalize deps that shouldn't be bundled into the library
         external: [
@@ -25,6 +45,12 @@ export default defineConfig(() => {
         ],
       },
     },
-    plugins: [qwikVite(), tsconfigPaths()],
+    plugins: [
+      dts({
+        exclude: ["src/entry.*.tsx", "src/root.tsx"],
+      }),
+      qwikVite(),
+      tsconfigPaths(),
+    ],
   };
 });

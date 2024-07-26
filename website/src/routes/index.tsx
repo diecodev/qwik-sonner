2bd5024b24a4bce361ee1a471b9f761a77152916
diecodev <diegoddiaz05@gmail.com>
2024-07-26 12:07:57 -0500

    docs(website): 📝 add note to avoid render location issues #13

--- website/src/routes/index.tsx
+++ website/src/routes/index.tsx
@@ -29,6 +29,20 @@ export default component$(() => {
       />
       <main class="container">
         <Hero />
+        <p class="!mt-12 rounded-md bg-amber-100 p-2 !text-sm">
+          Due to the resumability, the{" "}
+          <span class="rounded-lg border border-amber-200 bg-amber-200/65 p-1 font-mono text-xs font-semibold">{`<Toaster />`}</span>{" "}
+          component should be positioned at the beginning of where you want to
+          use it. This way, you won't encounter{" "}
+          <a
+            href="https://github.com/diecodev/qwik-sonner/issues/13"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="font-bold underline"
+          >
+            rendering issues.
+          </a>
+        </p>
         <div class="content">
           <Installation />
           <Usage />
